b3ab7b9d9a4fac9de74e5b8749b7b48ca0f87f54
Peter Ban <bn.peter.hun@gmail.com>
2026-04-25 23:57:28 +0200

    update game initialization

--- chess_engine/main.cpp
+++ chess_engine/main.cpp
@@ -2,11 +2,136 @@
 #include <vector>
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
+
+
+class Position {
+public:
+    int x, y;
+
+    Position(const int x, const int y) : x(x), y(y) {}
+    void print() const { std::cout << x << " " << y << std::endl; }
+    bool upgrade(const std::string& color, const std::string& name) {
+
+        if (color == "white" && name == "pawn" && y == 8) {
+            return true;
+            //std::cout << "White" << std::endl;
+        }
+        if (color == "black" && name == "pawn" && y == 0) {
+            return true;
+            //std::cout << "Black" << std::endl;
+        }
+
+
+        return false;
+    }
+
+
+
+};
+
+
+//TODO castle and double at generating moves, and upgrade as well
+
+class Piece {
+public:
+    Position position;
+    const std::string name;
+    bool moved = false;
+    const std::vector<std::vector<int>> moves;
+    Piece(const std::string& name, const Position position) : position(position), name(name) {
+    }
+
+    static void print_position(){}
+    Position get_position() const { return position; }
+    std::vector<std::vector<Position>> get_valid_moves() const {}
+
+    void set_position(const Position& pos) { position.x=pos.x; position.y=pos.y; }
+
+};
+
+class Player {
+public:
+    std::string name;
+    std::string color;
+    std::vector<Piece> pieces_pawns;
+    std::vector<Piece> pieces;
+    Player(const std::string& name, const std::string color) :  name(name), color(color) {
+    }
+    static void print_pieces(){}
+
+
+    std::vector<std::vector<Position>> get_positions() const {}
+
+    void init_pieces() {
+
+        int y = 1;
+        if (color == "white") {
+            y = 1;
+            pieces.push_back(Piece("King", Position(4,0)));
+            pieces.push_back(Piece("Queen", Position(5,0)));
+
+            pieces.push_back(Piece("Rook", Position(0,0)));
+            pieces.push_back(Piece("Knight", Position(1,0)));
+            pieces.push_back(Piece("Bishop", Position(3,0)));
+
+            pieces.push_back(Piece("Rook", Position(8,0)));
+            pieces.push_back(Piece("Knight", Position(7,0)));
+            pieces.push_back(Piece("Bishop", Position(6,0)));
+        }
+        else if (color == "black") {
+            y = 7;
+            pieces.push_back(Piece("King", Position(4,8)));
+            pieces.push_back(Piece("Queen", Position(5,8)));
+
+            pieces.push_back(Piece("Rook", Position(0,8)));
+            pieces.push_back(Piece("Knight", Position(1,8)));
+            pieces.push_back(Piece("Bishop", Position(3,8)));
+
+            pieces.push_back(Piece("Rook", Position(8,8)));
+            pieces.push_back(Piece("Knight", Position(7,8)));
+            pieces.push_back(Piece("Bishop", Position(6,8)));
+        }
+        std::cout << "Adding pawns" << std::endl;
+
+        for (int i = 0; i < 8; i++) {
+            pieces_pawns.push_back(Piece(color, Position(i,y)));
+        }
+
+
+
+
+
+    }
+};
+
+
 class Board {
 
+    public:
+        std::vector<std::vector<int>> board;
+        std::vector<std::vector<int>> free_spaces;
+        std::vector<Player> players;
 
-    std::vector<std::vector<int>> board;
-    std::vector<std::vector<int>> free_spaces;
+        const int min_x = 0;
+        const int min_y = 0;
+        const int max_x = 8;
+        const int max_y = 8;
+
+        const int number_of_players = 2;
+
+
+
+        void print() const {}
+
+
+        void initialize() {
+            players.push_back(Player("Player 1" , "white"));
+            players.push_back(Player("Player 2","black"));
+
+            for ( auto& player : players) {
+                player.init_pieces();
+            }
+        }
 
     //get valid moves, base on piece postions validate correct ones
 };
@@ -14,21 +139,17 @@ class Board {
 
 
 
-class Position {
-    public:
-        int x, y;
 
-    Position(const int x, const int y) : x(x), y(y) {}
-
-
-
-    void print() const { std::cout << x << " " << y << std::endl; }
-
-};
-
+//TODO check for capturing
 
 class GamedInitialization {
-    std::vector<std::vector<int>> moves = {{1, 2}, {-1,2}, {2,1},  {-2,1}  };
+
+    std::vector<std::vector<int>> moves_knight = {{1, 2}, {-1,2}, {2,1},  {-2,1}  };
+
+    std::vector<std::vector<int>> moves_pawn = {{1, 2}, {-1,2}, {2,1},  {-2,1}  };
+    std::vector<std::vector<int>> moves_king = {{1, 2}, {-1,2}, {2,1},  {-2,1}  };
+    std::vector<std::vector<int>> moves_queen= {{1, 2}, {-1,2}, {2,1},  {-2,1}  };
+
 
 
     static void pieceInitialization(const std::vector<Position>& pos) {
@@ -37,33 +158,15 @@ class GamedInitialization {
 
 };
 
+void game_loop(Board board) {
 
-class Piece {
-    public:
-        int x, y;
-        Position position;
-        std::string name;
-        const std::vector<std::vector<int>> moves;
-        Piece(const int x, const int y) : x(x), y(y), position(x,y) {
-            }
+    bool check_mate = false;
 
-        static void print_position(){}
-        Position get_position() const { return Position(x, y); }
-        std::vector<std::vector<Position>> get_valid_moves() const {}
+    while (!check_mate) {
 
-        void set_position(const Position& pos) { x=pos.x; y=pos.y; }
+    }
+}
 
-};
-
-class Player {
-public:
-    std::string name;
-    std::string color;
-    std::vector<std::vector<Piece>> pieces_pawns;
-    std::vector<std::vector<Piece>> pieces;
-    static void print_pieces(){}
-    std::vector<std::vector<Position>> get_positions() const {}
-};
 
 
 class Knight {
@@ -79,61 +182,6 @@ class Knight {
     static void show_piece_name() { std::cout << "Value: " << "Horse" << std::endl;}
 };
 
-class Bishop {
-    int x;
-    int y;
-    const std::vector<std::vector<int>> moves = {{2,1},  {2,-1},  {-2,1}, {-2,-1}, {1, 2}, {-1,2},{-1, -2}, {1,-2}  };
-
-    Bishop(const int x, const int y) : x(x), y(y) {}
-
-    //for example the position change of the rook is based on the intou
-    static void change_position(const int x, const int y) {}
-
-    static void show_piece_name() { std::cout << "Value: " << "Horse" << std::endl;}
-};
-
-
-
-class Rook {
-    int x;
-    int y;
-    const std::vector<std::vector<int>> moves = {{2,1},  {2,-1},  {-2,1}, {-2,-1}, {1, 2}, {-1,2},{-1, -2}, {1,-2}  };
-
-    Rook(const int x, const int y) : x(x), y(y) {}
-
-    //for example the position change of the rook is based on the intou
-    static void change_position(const int x, const int y) {}
-
-    static void show_piece_name() { std::cout << "Value: " << "Horse" << std::endl;}
-};
-
-
-class King {
-    int x;
-    int y;
-    const std::vector<std::vector<int>> moves = {{2,1},  {2,-1},  {-2,1}, {-2,-1}, {1, 2}, {-1,2},{-1, -2}, {1,-2}  };
-
-    King(const int x, const int y) : x(x), y(y) {}
-
-    //for example the position change of the rook is based on the intou
-    static void change_position(const int x, const int y) {}
-
-    static void show_piece_name() { std::cout << "Value: " << "Horse" << std::endl;}
-};
-
-
-class Queen {
-    int x;
-    int y;
-    const std::vector<std::vector<int>> moves = {{2,1},  {2,-1},  {-2,1}, {-2,-1}, {1, 2}, {-1,2},{-1, -2}, {1,-2}  };
-
-    Queen(const int x, const int y) : x(x), y(y) {}
-
-    //for example the position change of the rook is based on the intou
-    static void change_position(const int x, const int y) {}
-
-    static void show_piece_name() { std::cout << "Value: " << "Horse" << std::endl;}
-};
 
 class Pawn {
     int x;
@@ -150,7 +198,14 @@ class Pawn {
     static void show_piece_name() { std::cout << "Value: " << "Horse" << std::endl;}
 };
 
-
+/*
+ * TODO list
+ *
+ * Initialize game
+ * Initilaize players
+ * Initilize pieces
+ *
+ */
 int main() {
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
     auto lang = "C++";
@@ -161,6 +216,8 @@ int main() {
         std::cout << "i = " << i << std::endl;
     }
 
+    auto board = Board();
+    board.initialize();
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
